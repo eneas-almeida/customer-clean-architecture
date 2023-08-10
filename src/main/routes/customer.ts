db1dd711f3c5b8f44cab201540bf9fe023c91592
venzel <eneas.eng@yahoo.com>
2023-08-09 23:49:30 -0300

    feat: implementa as rotas, chamando as factories

--- src/main/routes/customer.ts
+++ src/main/routes/customer.ts
@@ -1,3 +1,15 @@
 import { Router } from 'express';
+import {
+    createControllerAdapter,
+    findOneControllerAdapter,
+    updateControllerAdapter,
+} from '../adapters/controllers/customer-controller.adapter';
+import { MakeCustomerController } from '../factories';
 
-export default async (router: Router): Promise<void> => {};
+export default async (router: Router): Promise<void> => {
+    const customerController = await MakeCustomerController();
+
+    router.post('/customers', createControllerAdapter(customerController));
+    router.patch('/customers/:id', updateControllerAdapter(customerController));
+    router.get('/customers/:id', findOneControllerAdapter(customerController));
+};
