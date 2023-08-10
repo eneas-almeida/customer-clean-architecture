6cdeba100597a6722cdb6fedcac2cfbbd5108d69
venzel <eneas.eng@yahoo.com>
2023-08-09 23:18:53 -0300

    feat: cria a camada presentation

---
+++ src/presentation/@shared/contracts/controllers.ts
@@ -0,0 +1,11 @@
+import { HttpResponse } from '../helpers/http';
+
+import { InputCreateCustomerDto, OutputCreateCustomerDto } from '@/usecase/customer/create';
+import { InputUpdateCustomerDto, OutputUpdateCustomerDto } from '@/usecase/customer/update';
+import { InputFindOneCustomerDto, OutputFindOneCustomerDto } from '@/usecase/customer/findone';
+
+export interface CustomerControllerInterface {
+    create: (input: InputCreateCustomerDto) => Promise<HttpResponse<OutputCreateCustomerDto>>;
+    update: (input: InputUpdateCustomerDto) => Promise<HttpResponse<OutputUpdateCustomerDto>>;
+    findOne: (input: InputFindOneCustomerDto) => Promise<HttpResponse<OutputFindOneCustomerDto>>;
+}
---
+++ src/presentation/controllers/customer.controller.ts
@@ -0,0 +1,41 @@
+import { CreateCustomerUseCase, FindOneCustomerUseCase, UpdateCustomerUseCase } from '@/usecase/customer';
+import { InputCreateCustomerDto, OutputCreateCustomerDto } from '@/usecase/customer/create';
+import { InputFindOneCustomerDto, OutputFindOneCustomerDto } from '@/usecase/customer/findone';
+import { InputUpdateCustomerDto, OutputUpdateCustomerDto } from '@/usecase/customer/update';
+import { CustomerControllerInterface } from '../@shared/contracts';
+import { HttpResponse, ok } from '../@shared/helpers';
+
+export class CustomerController implements CustomerControllerInterface {
+    constructor(
+        private readonly createCustomerUseCase: CreateCustomerUseCase,
+        private readonly updateCustomerUseCase: UpdateCustomerUseCase,
+        private readonly findOneCustomerUseCase: FindOneCustomerUseCase
+    ) {}
+
+    async create(input: InputCreateCustomerDto): Promise<HttpResponse<OutputCreateCustomerDto>> {
+        try {
+            const output = await this.createCustomerUseCase.execute(input);
+            return ok(output);
+        } catch (e) {
+            throw e;
+        }
+    }
+
+    async update(input: InputUpdateCustomerDto): Promise<HttpResponse<OutputUpdateCustomerDto>> {
+        try {
+            const output = await this.updateCustomerUseCase.execute(input);
+            return ok(output);
+        } catch (e) {
+            throw e;
+        }
+    }
+
+    async findOne(input: InputFindOneCustomerDto): Promise<HttpResponse<OutputFindOneCustomerDto>> {
+        try {
+            const output = await this.findOneCustomerUseCase.execute(input);
+            return ok(output);
+        } catch (e) {
+            throw e;
+        }
+    }
+}
